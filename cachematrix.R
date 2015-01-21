7044964bd526bd9810c4c62fefbaa05824b9047d
udotan <udotan@gmail.com>
2015-01-21 17:20:46 -0500

    Completed Assignment 2
    Got a working function set

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Setting a null matrix to store the cached results of the inverse of a matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) { 
+        s <- NULL
+        print(environment())
+        evn <- environment()
+        print(parent.env(evn))
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        getenv <- function() environment()
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve,
+             getenv   = getenv)
 
 }
 
 
-## Write a short comment describing this function
+## Pull inverse from chache if it is available
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
